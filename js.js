40e87fb69a13f62f6486007ef3f6bd2176f4a669
Vadim-Prishutov-QA <Vadimprishutov1992@yandex.ru>
2024-06-01 18:11:12 +0300

    Файлы сайта визитки

---
+++ js.js
@@ -0,0 +1,8 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('Ошибки — это возможность стать лучше')
+	cursor: url('/пусть к файлу/sword.cur'), auto;
+})
